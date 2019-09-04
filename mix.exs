1df4376f3ca584292986fd6ddf02f582392b0f75
Jamie Wright <jamie@brilliantfantastic.com>
2019-09-04 07:50:58 -0400

    Install absinthe

--- mix.exs
+++ mix.exs
@@ -33,6 +33,7 @@ defmodule Gotcha.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:absinthe_plug, "~> 1.4.0"},
       {:phoenix, "~> 1.4.9"},
       {:phoenix_pubsub, "~> 1.1"},
       {:phoenix_ecto, "~> 4.0"},
