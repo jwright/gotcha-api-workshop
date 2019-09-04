f8f2a02ef02877a28a96d13ad3ae230ace4306db
Jamie Wright <jamie@brilliantfantastic.com>
2019-09-04 18:30:36 -0500

    Revert "Initial GraphQL routes"
    This reverts commit 16b8299ebd8b565cc563bec196a13b3cb512ce95.

--- lib/gotcha_web/endpoint.ex
+++ lib/gotcha_web/endpoint.ex
@@ -29,9 +29,6 @@ defmodule GotchaWeb.Endpoint do
     pass: ["*/*"],
     json_decoder: Phoenix.json_library()
 
-  plug Absinthe.Plug,
-    schema: GotchaWeb.GraphQL.Schema
-
   plug Plug.MethodOverride
   plug Plug.Head
 
